d42c6515f15bd5d3ae74e68361f338e1d33fef39
lfarias- <leofariasrj25@gmail.com>
2022-08-11 15:13:46 -0300

    Refactor stack.h and stack.c, implement create_stk_node

--- stack.c
+++ stack.c
@@ -6,10 +6,24 @@
 /*   By: lfarias- <lfarias-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 22:03:26 by lfarias-          #+#    #+#             */
-/*   Updated: 2022/08/09 22:03:52 by lfarias-         ###   ########.fr       */
+/*   Updated: 2022/08/11 15:12:17 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stack.h"
+#include <stdlib.h>
 
-void	stck_add(
+t_stk_nd *create_stk_node(void *element)
+{
+	t_stk_nd	*new_node;
+
+	if (!element)
+		return (NULL);
+	new_node = malloc(sizeof(t_stk_nd));
+	if (!new_node)
+		return (NULL);
+	new_node->next = NULL;
+	new_node->previous = NULL;
+	new_node->content = element;
+	return (new_node);
+}
--- stack.h
+++ stack.h
@@ -6,7 +6,7 @@
 /*   By: lfarias- <lfarias-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 19:17:24 by lfarias-          #+#    #+#             */
-/*   Updated: 2022/08/09 21:43:43 by lfarias-         ###   ########.fr       */
+/*   Updated: 2022/08/11 15:11:27 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,19 @@
 
 typedef struct s_stack_node
 {
-	struct s_stack_node	previous;
-	struct s_stack_node	next;
-	void				*content;
+	struct	s_stack_node	*previous;
+	struct	s_stack_node	*next;
+	void					*content;
 } t_stk_nd;
 
 typedef struct s_stack
 {
-	struct s_stack_node first_node;
-	struct s_stack_node last_node;
+	t_stk_nd	*next_node;
+	t_stk_nd	*previous_node;
 } t_stack;
 
-void	stck_add(void *element, t_stack stack);
-void	*stck_pop(t_stack_stack);
-void	clean_stack(t_stack stack);
+t_stk_nd	*create_stk_node(void *element);
+void		stck_add(t_stk_nd new_node, t_stack stk);
+void		*stck_pop(t_stack stk);
+void		clean_stack(t_stack stk);
 # endif
