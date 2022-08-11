8c92863039f8510cb7d30390ec3f8ffafca5b4aa
lfarias- <leofariasrj25@gmail.com>
2022-08-11 19:38:49 -0300

    Add stck_add() unit tests

--- test_main.c
+++ test_main.c
@@ -6,7 +6,7 @@
 /*   By: lfarias- <lfarias-@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 15:31:34 by lfarias-          #+#    #+#             */
-/*   Updated: 2022/08/11 16:56:32 by lfarias-         ###   ########.fr       */
+/*   Updated: 2022/08/11 19:35:08 by lfarias-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*void	stck_add_test(t_stack node)
+void	stck_add_test()
 {
+	t_stk_nd	*c_node;
+	t_stack		*test_stack;
+	t_stack		*test_stack_2;
 
-}*/
+	printf("\n>>> stck_add_test\n");
+	printf("\n> Basic Functionality \n");
+	// test 1
+	// verificar se os dados estão empilhados da maneira correta
+	test_stack = malloc(sizeof(t_stack));
+	test_stack->top_node = NULL;
+	test_stack->bottom_node = NULL;
+	t_stk_nd *node = create_stk_node(ft_strdup("Item 1"));
+	stck_add(node, test_stack);
+	node = create_stk_node(ft_strdup("Item 2"));
+	stck_add(node, test_stack);
+	node = create_stk_node(ft_strdup("Item 3"));
+	stck_add(node, test_stack);
+	c_node = test_stack->bottom_node;
+	printf("> from bottom to top:\n");
+	while (c_node)
+	{
+		printf("[%p]: %s\n", c_node, c_node->content);
+		c_node = c_node->next;
+	}
+	printf("> from top to bottom:\n");
+	c_node = test_stack->top_node;
+	while (c_node)
+	{
+		printf("[%p]: %s\n", c_node, c_node->content);
+		c_node = c_node->previous;
+	}
+	
+	printf("\n > Add NULL node on populated stack\n");
+	stck_add(NULL, test_stack);
+	printf("expected: Item 3\n");
+	printf("got: %s\n", test_stack->top_node->content);
+	
+	printf("\n > Add NULL node on empty stack\n");
+	test_stack_2 = malloc(sizeof(t_stack));
+	test_stack_2->top_node = NULL;
+	test_stack_2->bottom_node = NULL;
+	stck_add(NULL, test_stack_2);
+	printf("top node: expected: 0x0 - got: %p\n", test_stack_2->top_node);
+	printf("bottom node: expected: 0x0 - got: %p\n", test_stack_2->bottom_node);
+
+	printf("\n > Add a valid NODE on a NULL stack\n");
+	printf("Non guarded code should provoke seg-fault\n");
+	c_node = create_stk_node(ft_strdup("Search and Destroy\n"));
+	stck_add(c_node, NULL);
+	c_node = create_stk_node(ft_strdup("Master of Puppets\n"));
+	stck_add(c_node, NULL);
+	printf("If it reached here then everything is ok!\n");
+
+	printf("\n > Add another stack on top of an existing stack \n");
+
+}
 	
 void	create_stk_node_test()
 {
@@ -55,4 +109,5 @@ int	main(void)
 {
 	printf("=== UNIT TESTS ===\n");
 	create_stk_node_test();
+	stck_add_test();
 }
